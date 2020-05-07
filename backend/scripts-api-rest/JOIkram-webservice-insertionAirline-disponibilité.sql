79689f64cc7e72b95769629a2f2254b21122d0f3
juliefino <64160383+juliefino@users.noreply.github.com>
2020-05-07 16:14:40 +0200

    Create JOIkram-webservice-insertionAirline-disponibilité.sql

---
+++ backend/scripts-api-rest/JOIkram-webservice-insertionAirline-disponibilité.sql
@@ -0,0 +1,27 @@
+/* Auteur : HE201844 Jaujate Ouldkhala Ikram
+Procedure qui permet d'ajouter à la base de donnée depuis HTML*/
+CREATE PROCEDURE "DBA"."ajout"(in id char(3), in nom char(50) )
+/* RESULT( nom_colonne type_colonne, ... ) */
+BEGIN
+Call sa_set_http_header('Access-Control-Allow-Origin', '*');
+	INSERT into Airline values( id , nom);
+END;
+
+
+
+CREATE SERVICE "TableInfo" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call proc_TableInfo(:code,:depart,:destination);
+
+
+
+
+/*Procedure qui réalise réquete afin de trouver l'information nécessaire sur chaque vol*/
+CREATE PROCEDURE "DBA"."proc_TableInfo"( codeVol INTEGER ,depart char(3), destination char(3) )
+RESULT (idCode INTEGER, name char(50), deptTime char(10),arrTime char(10), deptAirport char(50), destAirport char(50))
+BEGIN
+    Call sa_set_http_header('Access-Control-Allow-Origin', '*');
+	select DBA.Flight.idCode, DBA.Airline.name, DBA.Flight.deptTime, DBA.Flight.arrTime, getNomAirport(depart), getNomAirport(destination)
+        from DBA.Flight join Airline on Flight.idAirline = Airline.idAirline join Airports on DBA.Flight.deptAirport = idAirport
+        where deptAirport = depart and destAirport = destination
+END;
+
+CREATE SERVICE "ajout" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" URL ON METHO
